0bb85004a2de46b68f7b34cd2f6c305bfd3384da
sdm2182 <sdm2182@columbia.edu>
2020-05-15 13:26:25 -0400

    Completed Chess Page. Finished.

--- sdm2182-final/animations.py
+++ sdm2182-final/animations.py
@@ -4,6 +4,15 @@ Created on Wed May 13 17:43:55 2020
 
 @author: shane
 """
+#######################################################
+#   Name: Shane Maughn
+#   UNI: sdm2182
+#   
+#   This module houses functions which generate 2 
+#   animations.
+#
+######################################################
+
 from tkinter import *
 from time import sleep
 from PIL import Image, ImageDraw
@@ -12,18 +21,12 @@ from random import randint
 
 
 def race():
-    #tk = Tk()
-    #canvas = Canvas(tk, width=1000, height=500)
-    #tk.title("ENGI1006 Animation")
-    #canvas.pack()
 
+    #initializes PIL window
     img = Image.new("RGB",(1000,500))
     draw = ImageDraw.Draw(img)
 
-    #ball1 = canvas.create_oval(0, 0, 20, 20, fill = "#A0E5C9")
-    #ball2 = canvas.create_oval(0, 240, 20, 260, fill = "#629883")
-    #ball3 = canvas.create_oval(0, 480, 20, 500, fill = "#2B5444")
-
+    #initializes starting positions for 3 balls
     b1x1 = 0
     b1y1 = 0
     b1x2 = 20
@@ -37,85 +40,83 @@ def race():
     b3x2 = 20
     b3y2 = 500
     
+    #loop to simulate time
     for i in range(50):
-        #x1 = 20
-        #x2 = 1.5*i
-        #x3 = 0.2*(i**2)
-        
-        #canvas.move(ball1, x1, 0)
-        #canvas.move(ball2, x2, 0)
-        #canvas.move(ball3, x3, 0)
-        #tk.update()
-        sleep(0.04)
-        
+        #shifts the x-components of each ball by intended equation
         b1x1 += 20
         b1x2 += 20
         b2x1 += 1.5*i
         b2x2 += 1.5*i
         b3x1 += 0.2*(i**2)
         b3x2 += 0.2*(i**2)
-    
-        draw.rectangle([(0,0),(1000,500)], fill = (255,255,255))    
+        
+        #clears the screen
+        draw.rectangle([(0,0),(1000,500)], fill = (255,255,255)) 
+        #redraws new balls at updated locations
         draw.ellipse([(b1x1,b1y1),(b1x2,b1y2)], fill = (160,229,201))
         draw.ellipse([(b2x1,b2y1),(b2x2,b2y2)], fill = (98,152,131))
         draw.ellipse([(b3x1,b3y1),(b3x2,b3y2)], fill = (43,84,68))
-    
+        
+        #saves current window as an image
         filename = "static/animations/race" + str(i) +".png"
         img.save(filename)
 
-    #tk.mainloop()
 
 def race_gif():
+    
     frames = []
     
+    #opens each image and concatenates it to the frames list
     for i in range(50):
         filename = "static/animations/race" + str(i) + ".png"
         new_frame = Image.open(filename)
         frames.append(new_frame)
-
+    
+    #saves the images together as a gif
     frames[0].save('static/race.gif', save_all = True, append_images = frames[1:], 
            optimize = False, duration = 50, loop = 0)
     
 def explosion():
     
+    #initializes PIL window
     img = Image.new("RGB",(500,500))
     draw = ImageDraw.Draw(img)
     
+    #randomly generates the 6 centers for the explosion
     centers = [None]*12
     for i in range(len(centers)):
         centers[i] = randint(0, 500)
     
-        
-
-        
+    #initializes variables to house the angle each fragment leaves the explosion
+    #and the corresponding x and y components of its unit vector
     angles = [None]*10
     x_shift = [None]*10
     y_shift = [None]*10
+    
+    #randomly generates the 10 angles and calculates corresponding values for
+    #x_shift and y_shift
     for i in range(len(angles)):
         temp = randint(0, 360)
         angles[i] = temp * (6.283/360)
         x_shift[i] = int(10 * cos(angles[i]))
         y_shift[i] = int(10 * sin(angles[i]))
         
+    #initializes arrays for the shards from each explosion
+    #first co-ordinates
     ex1 = [None]*10
     ex2 = [None]*10
     ex3 = [None]*10
     ex4 = [None]*10
     ex5 = [None]*10
     ex6 = [None]*10
+    #second co-ordinates
     ex01 = [None]*10
     ex02 = [None]*10
     ex03 = [None]*10
     ex04 = [None]*10
     ex05 = [None]*10
     ex06 = [None]*10
-    
-    shift1 = [None]*2
-    shift2 = [None]*2
-    shift3 = [None]*2
-    shift4 = [None]*2
-    shift5 = [None]*2
-    shift6 = [None]*2
+
     
     for i in range(len(ex1)):
         ex1[i] = [centers[0],centers[1]]
@@ -133,9 +134,9 @@ def explosion():
         ex06[i] = [centers[10]+5,centers[11]+5]
 
     
-    
+    #loop to simulate time
     for i in range(30):
-        
+        #changes background based on time in a haphazard way
         if i % 2 == 0:
             draw.rectangle([(0,0),(1000,500)], fill = (199,211,203))
         elif i % 3 == 0:
@@ -146,6 +147,7 @@ def explosion():
             draw.rectangle([(0,0),(1000,500)], fill = (217,225,220))
             
         for j in range(len(ex1)):
+            #adjusts first co-ordinate for each shard based on desired motion
             ex1[j][0] += x_shift[j]*i*0.1
             ex1[j][1] += y_shift[j]*i*0.1
             ex2[j][0] += x_shift[j]*i*0.8
@@ -159,6 +161,7 @@ def explosion():
             ex6[j][0] += x_shift[j]*(i**5)*(5*10**-6)
             ex6[j][0] += y_shift[j]*(i**5)*(5*10**-6)
             
+            #adjusts second co-ordinate for each shard based on desired motion
             ex01[j][0] += x_shift[j]*i*0.1
             ex01[j][1] += y_shift[j]*i*0.1
             ex02[j][0] += x_shift[j]*i*0.8
@@ -172,6 +175,7 @@ def explosion():
             ex06[j][0] += x_shift[j]*(i**5)*(5*10**-6)
             ex06[j][1] += y_shift[j]*(i**5)*(5*10**-4)
             
+            #redraws each ellipse
             draw.ellipse([(ex1[j][0],ex1[j][1]),(ex01[j][0],ex01[j][1])], fill = (248,249,218))
             draw.ellipse([(ex2[j][0],ex2[j][1]),(ex02[j][0],ex02[j][1])], fill = (243,243,231))
             draw.ellipse([(ex3[j][0],ex3[j][1]),(ex03[j][0],ex03[j][1])], fill = (243,243,231))
@@ -179,7 +183,7 @@ def explosion():
             draw.ellipse([(ex5[j][0],ex5[j][1]),(ex05[j][0],ex05[j][1])], fill = (243,243,231))
             draw.ellipse([(ex6[j][1],ex6[j][0]),(ex06[j][1],ex06[j][0])], fill = (243,243,231))
         
-        
+        #saves current window as an image
         filename ='static/animations/explosion' + str(i) + '.png' 
         img.save(filename)
         
@@ -188,10 +192,12 @@ def explosion_gif(output_name):
     
     frames = []
     
+    #opens each image and concatenates it to the frames list
     for i in range(30):
         filename = 'static/animations/explosion' + str(i) + '.png'
         new_frame = Image.open(filename)
         frames.append(new_frame)
-
+    
+    #saves the frames list as a gif
     frames[0].save(output_name, save_all = True, append_images = frames[1:], 
            optimize = False, duration = 90, loop = 0)
--- sdm2182-final/app.py
+++ sdm2182-final/app.py
@@ -4,10 +4,22 @@ Created on Tue Apr 21 14:57:17 2020
 
 @author: etill
 """
+#######################################################
+#   Name: Shane Maughn
+#   UNI: sdm2182
+#   
+#   This program uses Flask to serve a main webpage,
+#   an animations page and a chess page
+#
+######################################################
 
 #import statements
 from flask import Flask, render_template
 from animations import  race, race_gif, explosion, explosion_gif
+import datetime, time
+import pandas as pd
+import requests
+import bs4
 
 #Flask app variable
 app = Flask(__name__)
@@ -23,12 +35,80 @@ def animate():
 
 @app.route("/chess")
 def chess():
-    return render_template("sdm2182_chess.html")
+
+    #store current time in the format hour: minute: time    
+    time = str(datetime.datetime.now().hour) + ':' + \
+    str(datetime.datetime.now().minute) + ':' + str(datetime.datetime.now().second)
+    
+    #Generates welcome message based on time of day
+    x = datetime.datetime.now().hour
+    if x < 12:
+        message = "Good Morning Shane. Here is your update:"
+    elif x < 16:
+        message = "Good Afternoon Shane. Here is your update:"
+    elif x < 20:
+        message =  "Good Evening Shane. Here is your update:"
+    else:
+         message = "Good Night Shane. Here is your update:"
+         
+    #converts ranking info from webpage into pandas DataFrame
+    country_info = bs4.BeautifulSoup\
+    (requests.get('https://ratings.fide.com/topfed.phtml?tops=0&ina=2&country=BAR')\
+     .content,'lxml')
+    country_tables = country_info.find_all('table')
+    temp = pd.read_html(str(country_tables[1]))
+    rankings = temp[4]
+    
+    #finds current ranking 
+    pos = int(rankings[rankings[1]=='Maughn, Shane'].index.values)
+    
+    #creates DataFrames for those players closest above and closest below
+    threats= pd.DataFrame()
+    threats["Name"] = rankings.iloc[pos+1:pos+4,1]
+    threats["Rating"] = rankings.iloc[pos+1:pos+4,4]
+    
+    targets= pd.DataFrame()
+    targets["Name"] = rankings.iloc[pos-3:pos,1]
+    targets["Rating"] = rankings.iloc[pos-3:pos,4]
+    
+    #converts personal chess statistics into a pandas DataFrame
+    personal_info =bs4.BeautifulSoup\
+    (requests.get('https://ratings.fide.com/card.phtml?event=11103310').content,'lxml')
+    personal_tables = personal_info.find_all('table')
+    data = pd.read_html(str(personal_tables[5]))
+    temp = data[0].iloc[3,2]
+    temp = temp.split()
+    personal_data = pd.DataFrame([["Std. Rating:", "Blitz Rating:", "Title:", "Fed:"],\
+                                 [temp[0][4:],temp[3][5:],data[0].iloc[2,2],data[0].iloc[1,2]]]\
+                                 ,index = ["0","1"]).T
+
+    #converts tournaments listing into a pandas DataFrame
+    tournament_info =bs4.BeautifulSoup\
+    (requests.get('https://ratings.fide.com/tournament_list.phtml?moder=ev_code&country=USA')\
+     .content,'lxml')
+    tournament_tables = tournament_info.find_all('table')
+    temp = pd.read_html((str(tournament_tables[1])))
+    
+    #selects the 5 closest tournaments to display
+    tournaments = temp[3].iloc[1:6, 2:4]
+    tournaments.columns = ["Name","Location"] 
+         
+    #returns required variables to html file
+    pos=str(pos)
+    return render_template("sdm2182_chess.html", time=time, message=message, \
+                           pos=pos, threats=threats.to_html(classes='data'), \
+                           targets=targets.to_html(classes='data'), \
+                           personal_data=personal_data, \
+                           tournaments=tournaments.to_html(classes='data'))
+ 
+
 
 #start the server
 if __name__ == "__main__":
     
     print("Animations Generating...")
+    
+    #generates animations and saves them as gifs
     race()
     race_gif()
     explosion()
